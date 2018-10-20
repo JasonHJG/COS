e61b6be651d7e1fe4342bbefcd197b8002357ccd
JasonHJG <jinganghe@g.ucla.edu>
2018-10-20 16:26:35 -0400

    update

--- Simulation/dummy_liquidity_process.py
+++ Simulation/dummy_liquidity_process.py
@@ -1,22 +1,28 @@
+import numpy as np
+
+
 class Dummy_liquidity_process:
     """
     case for a dummy liquidity process, the process is normalized and for simplicity assume to be 0.5
     """
 
-    def __init__(self, time_step, time):
+    def __init__(self, time):
         """
         initialize the constant liquidity process
-        :param time_step: float, unit time
         :param time: float, total time span
         """
-        n_step = int(time / time_step)
-        self.liquidity_process = np.ones(n_step) * 0.5
+        self.liquidity_dict = {}
+        n_step = int(time)
+        liquidity_process = np.ones(n_step) * 0.5
+        for i in range(len(liquidity_process)):
+            self.liquidity_dict[i] = liquidity_process[i]
 
     def move_forward(self):
         """
         move forward for one step
         """
-        self.liquidity_process.append(0.5)
+        last_time_step = list(self.liquidity_dict)[-1]
+        self.liquidity_dict[last_time_step+1] = 0.5
 
     def get_process(self, n_steps):
         """
@@ -24,4 +30,4 @@ class Dummy_liquidity_process:
         :param n_steps: int
         :return: list of recent n steps of liquidity data
         """
-        return self.liquidity_process[-n_steps:]
+        return [.5] * n_steps
