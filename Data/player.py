bcf65aa3764954d11af266b205ff8031aafd7d89
JasonHJG <jinganghe@g.ucla.edu>
2018-10-22 10:52:44 -0400

    update

--- Data/player.py
+++ Data/player.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Player:
     """
     define the player, who is able to buy and sell a stock he is holding
@@ -5,36 +8,67 @@ class Player:
     the player needs to record each step the state and action
     """
 
-    def __init__(self, stock, utility_function, strategy, action=[-200, -100, 0, 100, 200]):
+    def __init__(self, stock, utility_function, strategy, trade_book, action=[-200, -100, 0, 100, 200]):
         """
         initialize an instance of player in the stock market
         :param stock: stock the player is holding
         :param utility_function: the utility function of the player
         :param strategy: the RL strategy of the player
+        :param trade_book: a book to keep track of the player's position and cash
         :param action: list of possible movement of position
         """
         self.stock = stock
         self.utility_function = utility_function
         self.strategy = strategy
         self.action = action
-        # dictionary to record what actions are taken and what are the pnls
-        self.position = {}
-        self.pnl = {}
-        self.action_list = {}
+        self.trade_book = trade_book
 
-    def trade_epsilon(self, epsilon=0.05):
+    def observe(self, n_steps=1):
+        """
+        the player observe n_step backward to help make the decision
+        :param n_steps: [1] int number of steps to look back in the history of stock
+        :return: index list, price list, liquidity list
+        """
+        return self.stock.get_history(n_steps)
+
+    def record(self, time_step, action, price, trade_cost):
+        """
+        record the trade to the trade book
+        :param time_step: index for time step
+        :param action: share traded
+        :param price: price for each share
+        :param trade_cost: trade cost defined by the stock
+        """
+        self.trade_book.add(time_step, action, price, trade_cost)
+
+    def progress(self):
+        """
+        the stock the player is observing moves one step forward
+        """
+        self.stock.move_forward()
+
+    def epsilon_greedy(self, epsilon=0.05):
         """
         trade according to epsilon greedy algorithm:
         with probability epsilon to trade randomly,
-        with probability 1-epsilon not to trade
+        with probability 1-epsilon not to trade (action = 0)
         :param epsilon: probability to trade randomly
         """
-        
+        index, price, liquidity = self.observe(1)
+        index = index[0]
+        price = price[0]
+        liquidity = liquidity[0]
+        ran = np.random.random()
+        if ran < epsilon: # trade randomly
+            rand_index = np.random.randint(len(self.action))
+            action = self.action[rand_index]
+        else:
+            action = self.strategy.decide()
+        self.record(index, action, price, self.stock.trade_cost)
 
-
-    def make_strategy(self, look_back, length_of_state=1):
+    def update_strategy(self, look_back, length_of_state=1):
         """
-        make a strategy based on past observations
+        update Q function in strategy based on past observations
         :param look_back: int, number of steps to look back
         :param length_of_state: [1], number of steps to take as one state
         """
@@ -42,26 +76,3 @@ class Player:
         # todo implement strategy class
         self.strategy.train(index, price_list, liquidity_list, self.position, self.utility_function,
                             length_of_state, self.action)
-
-    def make_decision(self, length_of_state=1):
-        """
-        choose an action from action list
-        :param length_of_state: [1], number of steps to take as one state
-        :return: int, one action from action_list
-        """
-        price_list, liquidity_list = self.observe(length_of_state)
-        return self.strategy.decide()
-
-    def observe(self, n_steps):
-        """
-        the player observe n_step backward to help make the decision
-        :param n_steps: int number of steps to look back in the history of stock
-        :return: price list, liquidity list
-        """
-        return self.stock.get_history(n_steps)
-
-    def calculate_pnl(self):
-        """
-        calculate the pnl from the previous time step
-        :return: float, pnl from the previous day
-        """
--- Data/trade_book.py
+++ Data/trade_book.py
@@ -2,15 +2,17 @@ class Trade_book:
     """
     keep a record of all the transactions and the net worth
     """
-    def __init__(self, price, position, cash):
+    def __init__(self, time_step, price, position, cash, action):
         """
         initialize an instance of trade book
+        :param time_step: time step for the information
         :param price: float, price of each share of a stock
         :param position: int, position of the stock
         :param cash: float, cash
+        :param action: int, change in position
         """
         self.book = {}
-        self.book[0] = [price, position, cash]
+        self.book[time_step] = [price, position, cash]
 
     def net_worth(self):
         """
@@ -24,11 +26,24 @@ class Trade_book:
             net_worth[time] = price * position + cash
         return net_worth
 
-    def update(self, action, price, trade_cost):
+    def get_recent_information(self):
         """
-        update the trade book when an action is taken
-        :param action: int, change in the position
-        :param price: float, current stock price
-        :param trade_cost: function,
-        :return:
+        get the most recent information
+        :return: time_step, price, position, cash
         """
+        time_steps = list(self.book)
+        time_steps.sort()
+        recent_time_step = time_steps[-1]
+        price, position, cash, action = self.book[recent_time_step]
+        return recent_time_step, price, position, cash, action
+
+    def add(self, time_step, action, price, trade_cost):
+        """
+        record the trade to the trade book, adjust cash according to trading cost
+        :param time_step: index for time step
+        :param action: share traded
+        :param price: price for each share
+        :param trade_cost: trade cost defined by the stock
+        """
+        _, last_price, last_postion, last_cash, _ = self.get_recent_information()
+        self.book[time_step] = [price, last_postion+action, last_cash - trade_cost(action), action]
